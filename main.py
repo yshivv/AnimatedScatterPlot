e469a609b19a51f7c0639179f29eadc4f7d538cd
shivpalyadavv <shivpaly2@gmail.com>
2022-11-18 17:58:42 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+rng = np.random.RandomState(0)
+x = rng.randn(100)
+y = rng.randn(100)
+colors = rng.rand(100)
+sizes = 1000 * rng.rand(100)
+
+plt.scatter(x, y, c=colors, s=sizes, alpha=0.3,
+            cmap='viridis')
+plt.colorbar()
+plt.show()
